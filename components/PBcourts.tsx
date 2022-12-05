bf26e2af11a4e3032650864a6a5a876c4ad09414
gleasonw <wgleason9@gmail.com>
2022-12-04 20:55:19 -0800

    cache experiments

--- components/PBcourts.tsx
+++ components/PBcourts.tsx
@@ -2,11 +2,22 @@ import React from "react";
 import Image from "next/image";
 
 export const PBcourts: React.FC = () => {
-  const [time, setTime] = React.useState(new Date().toLocaleTimeString());
+  const [nextCacheAge, setNextCacheAge] = React.useState(new Date());
+
+  //ensure that the cache is updated every 5 minutes?
+
+  React.useEffect(() => {
+    const now = new Date();
+    if (now.getTime() - nextCacheAge.getTime() > 300000) {
+      console.log('old cache');
+      setNextCacheAge(now);
+    }
+  }, [nextCacheAge]);
+
   return (
     <div className="w-4/5 mx-auto">
       <Image
-        src={`https://drive.google.com/uc?export=view&id=1hNQIivHi6k8yCQe3uc-J8ghBQzJqipmk&time=${time}`}
+        src={`https://drive.google.com/uc?export=view&id=1hNQIivHi6k8yCQe3uc-J8ghBQzJqipmk&time=${nextCacheAge}`}
         width={1000}
         height={500}
         alt="PB Courts"
