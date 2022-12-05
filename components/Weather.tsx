227bc58744deaa5edd31298179265fa955a69097
Will Gleason <wgleason9@gmail.com>
2022-12-04 16:19:50 -0800

    styles

--- components/Weather.tsx
+++ components/Weather.tsx
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { ReactComponentElement } from "react";
 import useSWR from "swr";
 import styles from "../styles/Home.module.css";
 import HourForecast from "./HourForecast";
 import fetcher from "./fetcher";
 import Image from "next/image";
-
 
 interface ForecastPeriod {
   number: number;
@@ -32,16 +31,20 @@ export const Weather: React.FC = () => {
   const forecast: ForecastPeriod[] = data.properties.periods;
   const focusForecast: ForecastPeriod = forecast[0];
 
+  const icon = (url: string, alt: string) => (
+    <Image src={url} height={100} width={100} alt={alt} className="mx-auto" />
+  );
+
   return (
     <div className="flex flex-col justify-center items-center">
-      <HourForecast />
       <div className={styles.card}>
         <h1 className="text-3xl">{focusForecast.name}</h1>
         <p className="text-4xl">{focusForecast.temperature}</p>
         <p>{focusForecast.windSpeed}</p>
         <p>{focusForecast.shortForecast}</p>
-        <Image src={focusForecast.icon} height={50} width={50} alt={focusForecast.shortForecast} />
+        {icon(focusForecast.icon, focusForecast.shortForecast)}
       </div>
+      <HourForecast />
       <div className={"flex flex-wrap"}>
         {forecast.slice(1).map((forecast: ForecastPeriod) => {
           if (forecast.isDaytime) {
@@ -51,7 +54,7 @@ export const Weather: React.FC = () => {
                 <p>{forecast.temperature}</p>
                 <p>{forecast.windSpeed}</p>
                 <p>{forecast.shortForecast}</p>
-                <Image height={50} width={50} src={forecast.icon} alt={forecast.shortForecast} />
+                {icon(forecast.icon, forecast.shortForecast)}
               </div>
             );
           }
