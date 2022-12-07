089da62af986b28b8913b7f5aa1e4297c1fc0374
Will Gleason <wgleason9@gmail.com>
2022-12-07 09:57:14 -0800

    bg black

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { Weather } from "./components/Weather";
 
 export default function Home() {
   return (
-    <div className={"flex-col flex justify-center bg-black pt-10 text-xl"}>
+    <div className={"flex-col flex justify-center pt-10 text-xl"}>
       <PBcourts />
       <Weather />
     </div>
