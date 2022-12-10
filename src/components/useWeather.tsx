87a1af8fb407942748c17b98b00edb19b7670447
gleasonw <wgleason9@gmail.com>
2022-12-10 07:55:45 -0800

    retries?

--- src/components/useWeather.tsx
+++ src/components/useWeather.tsx
@@ -26,12 +26,27 @@ function useWeather(url: string): {
   data: ForecastBatch | undefined;
   error: Error | undefined;
 } {
+
+  type HttpError = Error & { status?: number };
+
   const { data, error } = useSWR<ForecastBatch, Error>(url, async (url) => {
     const res = await fetch(url);
     const data = await res.json();
     console.log(data);
     return data;
-  });
+  }, {
+    onErrorRetry: (error: HttpError, key, config, revalidate, { retryCount }) => {
+      // Never retry on 404.
+      if (error.status === 404) return;
+
+      // Only retry up to 10 times.
+      if (retryCount >= 10) return;
+
+      // Retry after 5 seconds.
+      setTimeout(() => revalidate({ retryCount }), 5000);
+    }
+  }
+  );
 
   return { data, error };
 }
